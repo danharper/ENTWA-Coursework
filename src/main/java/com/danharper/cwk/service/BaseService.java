f8e075b9685f83de2d562618ebd66568c2f3c1a5
Dan Harper <intouch@danharper.me>
2013-03-18 22:52:09 +0000

    aww yeah, base service

--- src/main/java/com/danharper/cwk/service/BaseService.java
+++ src/main/java/com/danharper/cwk/service/BaseService.java
@@ -1,8 +1,53 @@
 package com.danharper.cwk.service;
 
+import com.danharper.cwk.facade.AbstractFacade;
+import java.util.List;
+
 public abstract class BaseService<T>
 {
-    public abstract T findById(Long id);
-    public abstract void persist(T entity);
-    public abstract void merge(T entity);
+    
+    private Class<T> entityClass;
+
+    public BaseService(Class<T> entityClass)
+    {
+        this.entityClass = entityClass;
+    }
+    
+    protected abstract AbstractFacade<T> getFacade();
+    
+    public T find(Long id)
+    {
+        return getFacade().find(id);
+    }
+    
+    public List<T> findAll()
+    {
+        return getFacade().findAll();
+    }
+    
+    public T create(T entity)
+    {
+        return getFacade().create(entity);
+    }
+    
+    public T update(T entity)
+    {
+        return getFacade().update(entity);
+    }
+    
+    public void remove(T entity)
+    {
+        getFacade().remove(entity);
+    }
+    
+    public long count()
+    {
+        return getFacade().count();
+    }
+    
+    public List<T> findRange(int currentPage, int pageSize)
+    {
+        return getFacade().findRange(currentPage, pageSize);
+    }
+    
 }
--- src/main/java/com/danharper/cwk/service/IdeaService.java
+++ src/main/java/com/danharper/cwk/service/IdeaService.java
@@ -1,8 +1,8 @@
 package com.danharper.cwk.service;
 
 import com.danharper.cwk.domain.Idea;
+import com.danharper.cwk.facade.AbstractFacade;
 import com.danharper.cwk.facade.IdeaFacade;
-import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
@@ -11,44 +11,19 @@ import javax.ejb.Stateless;
  * @author danharper
  */
 @Stateless
-public class IdeaService {
+public class IdeaService extends BaseService<Idea> {
+    
+    public IdeaService()
+    {
+        super(Idea.class);
+    }
     
     @EJB
     private IdeaFacade ideas;
     
-    public Idea find(Long id)
+    protected AbstractFacade<Idea> getFacade()
     {
-        return ideas.find(id);
-    }
-    
-    public List<Idea> findAll()
-    {
-        return ideas.findAll();
-    }
-    
-    public Idea create(Idea idea)
-    {
-        return ideas.create(idea);
-    }
-    
-    public Idea update(Idea idea)
-    {
-        return ideas.update(idea);
-    }
-    
-    public void remove(Idea idea)
-    {
-        ideas.remove(idea);
-    }
-    
-    public long count()
-    {
-        return ideas.count();
-    }
-    
-    public List<Idea> findRange(int currentPage, int pageSize)
-    {
-        return ideas.findRange(currentPage, pageSize);
+        return this.ideas;
     }
     
 }
