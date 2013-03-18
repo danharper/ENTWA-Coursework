15e90fd47e6e51902a2d768723357ad3ea4e7018
Dan Harper <intouch@danharper.me>
2013-03-18 22:57:20 +0000

    person service using abstract

--- src/main/java/com/danharper/cwk/service/PersonService.java
+++ src/main/java/com/danharper/cwk/service/PersonService.java
@@ -1,8 +1,8 @@
 package com.danharper.cwk.service;
 
 import com.danharper.cwk.domain.Person;
+import com.danharper.cwk.facade.AbstractFacade;
 import com.danharper.cwk.facade.PersonFacade;
-import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
@@ -11,44 +11,19 @@ import javax.ejb.Stateless;
  * @author danharper
  */
 @Stateless
-public class PersonService {
+public class PersonService extends AbstractService<Person> {
+    
+    public PersonService()
+    {
+        super(Person.class);
+    }
     
     @EJB
     private PersonFacade people;
     
-    public Person find(Long id)
+    protected AbstractFacade<Person> getFacade()
     {
-        return people.find(id);
-    }
-    
-    public List<Person> findAll()
-    {
-        return people.findAll();
-    }
-    
-    public Person create(Person person)
-    {
-        return people.create(person);
-    }
-    
-    public Person update(Person person)
-    {
-        return people.update(person);
-    }
-    
-    public void remove(Person person)
-    {
-        people.remove(person);
-    }
-    
-    public long count()
-    {
-        return people.count();
-    }
-    
-    public List<Person> findRange(int currentPage, int pageSize)
-    {
-        return people.findRange(currentPage, pageSize);
+        return this.people;
     }
     
 }
